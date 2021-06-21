2f75010016e9103ea103b484eb78fbe26a92b954
unknown <nguyendongminhtri29092014@gmail.com>
2021-06-21 18:32:22 +0700

    change color button

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,36 @@
+import {Component, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {RouterModule} from '@angular/router';
+import {ThemePickerModule} from '../theme-picker';
+import {ThemeStorage} from '../theme-picker/theme-storage/theme-storage';
+import {StyleManager} from '../style-manager';
+import {HttpClientModule} from '@angular/common/http';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.scss']
+})
+export class NavBar {
+  newColor = false;
+  toggleColor() {
+    this.newColor = !this.newColor;
+  }
+}
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterModule,
+    ThemePickerModule,
+  ],
+  exports: [NavBar],
+  declarations: [NavBar],
+  providers: [StyleManager, ThemeStorage]
+})
+export class NavBarModule {}
